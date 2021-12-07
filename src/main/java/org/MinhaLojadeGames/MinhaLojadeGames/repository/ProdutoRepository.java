3ef4eacbe096e5eb1aceb5a127ecc9336524b2d9
Bruna <bruna41bergami@gmail.com>
2021-12-07 11:46:49 -0300

    @bruna41 [Task: Criando model e repository produto]

--- src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/repository/ProdutoRepository.java
+++ src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/repository/ProdutoRepository.java
@@ -1,5 +1,12 @@
 package org.MinhaLojadeGames.MinhaLojadeGames.repository;
 
-public interface ProdutoRepository {
+import java.util.List;
 
+import org.MinhaLojadeGames.MinhaLojadeGames.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+	public List<Produto> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
+	
 }
