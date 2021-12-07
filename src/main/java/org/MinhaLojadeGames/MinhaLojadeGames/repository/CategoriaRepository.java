ea62aa5304f710828c0fbbcf60a5e71c50a06b0a
Otavio Tonello <otaviotonello0102@gmail.com>
2021-12-07 12:04:18 -0300

    @otaviotonello [Task: Adicionando Produto Controller]

--- src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/repository/CategoriaRepository.java
+++ src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/repository/CategoriaRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.MinhaLojadeGames.MinhaLojadeGames.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 	public List<Categoria> findAllByNomeCategoriaContainingIgnoreCase(String nomeCategoria);
 
