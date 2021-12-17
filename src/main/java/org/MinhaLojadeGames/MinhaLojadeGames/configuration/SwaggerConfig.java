19165084abf69f3dc9c6c2c74236a826e48e325f
Bruna <bruna41bergami@gmail.com>
2021-12-17 10:04:34 -0300

    @bruna41 [Task: Criando class swagger]

--- src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/configuration/SwaggerConfig.java
+++ src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/configuration/SwaggerConfig.java
@@ -1,5 +1,33 @@
 package org.MinhaLojadeGames.MinhaLojadeGames.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
 public class SwaggerConfig {
 
+	@Bean
+	public OpenAPI springLojaeGamesOpenAPI () {
+		return new OpenAPI()
+					.info(new Info()
+						.title ("Loja de Games")
+						.description ("Loja de Games - Generation Brasil")
+						.version ("v0.0.1")
+					.license(new License()
+								.name("Generation Brasil")
+								.url("http://brazil,genaeration,org/"))
+					.contact (new Contact()
+								.name("Loja de Games")
+								.url("https://github.com/pedro-borelli/LojaDeGames")
+								.email("pedroborelli@gmail.com")))
+					.externalDocs (new ExternalDocumentation()
+							.description("GirHub")
+							.url("https://github.com/pedro-borelli/LojaDeGames"));
+	}
 }
