c246416ae36782a6f3154d7a335e73afc3bc1571
unknown <borellipedroluiz@gmail.com>
2021-12-17 08:46:24 -0300

    @pedro-borelli [Task: Adicionando pasta UsuarioService]

---
+++ src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/service/UsuarioService.java
@@ -0,0 +1,62 @@
+package org.MinhaLojadeGames.MinhaLojadeGames.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.MinhaLojadeGames.MinhaLojadeGames.model.Usuario;
+import org.MinhaLojadeGames.MinhaLojadeGames.model.UsuarioLogin;
+import org.MinhaLojadeGames.MinhaLojadeGames.repository.UsuarioRepository;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+
+	public ResponseEntity<Usuario> cadastrarUsuario(Usuario usuario) {
+		Optional<Usuario> optional = repository.findByUsuario(usuario.getUsuario());
+		
+		if (optional.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email ja existe!");
+		} else {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
+			
+			return ResponseEntity.status(201).body(repository.save(usuario));
+		}
+		
+	}
+	
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
+		if(usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(),usuario.get().getSenha())) {
+				
+				String auth= user.get().getUsuario() + ":" + user.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic" + new String(encodedAuth);
+				
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				
+				return user;
+			}
+		}
+		
+		return null;
+		
+	}
+	
+}
+	
+
