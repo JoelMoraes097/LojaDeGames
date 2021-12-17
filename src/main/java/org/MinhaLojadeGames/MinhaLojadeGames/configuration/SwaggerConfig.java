188dad14cff9e2fa69b7eb23878d68c635418b58
Bianca <biancasouzarocha@outlook.com>
2021-12-17 12:17:39 -0300

    @BiancaSouzaRocha [Task: Atualizando Swagger Configuration]

--- src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/configuration/SwaggerConfig.java
+++ src/main/java/org/MinhaLojadeGames/MinhaLojadeGames/configuration/SwaggerConfig.java
@@ -1,13 +1,18 @@
 package org.MinhaLojadeGames.MinhaLojadeGames.configuration;
 
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 
 @Configuration
 public class SwaggerConfig {
@@ -29,5 +34,27 @@ public class SwaggerConfig {
 					.externalDocs (new ExternalDocumentation()
 							.description("GitHub")
 							.url("https://github.com/pedro-borelli/LojaDeGames"));
+	}
+	
+	@Bean
+	public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser () {
+		return OpenApiCustomiser -> {
+			OpenApiCustomiser.getPaths().values().forEach(PathItem -> PathItem.readOperations().forEach(operation -> {
+				
+				ApiResponses apiResponses = operation.getResponses();
+				
+				apiResponses.addApiResponse("200", createApiResponse("Sucesso!"));
+				apiResponses.addApiResponse("201", createApiResponse("Objeto Persistido!"));
+				apiResponses.addApiResponse("204", createApiResponse("Objeto Excluído!"));
+				apiResponses.addApiResponse("400", createApiResponse("Erro na Requisição!"));
+				apiResponses.addApiResponse("401", createApiResponse("Acesso Não Autorizado!"));
+				apiResponses.addApiResponse("404", createApiResponse("Objeto Não Encontrado!"));
+				apiResponses.addApiResponse("500", createApiResponse("Erro na Aplicação!"));
+			}));
+		};
+	}
+
+	private ApiResponse createApiResponse (String message) {
+		return new ApiResponse().description(message); 
 	}
 }
